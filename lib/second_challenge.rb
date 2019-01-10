d601a4cb16ae5a6b84b8bc5d8710f2c578837a89
David Charles <ratlover03@gmail.com>
2019-01-10 01:26:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,7 @@ def second_challenge
       :hero_friends => []
       },
     :capulet => {
-      :patriarch => {name: "Lady Capulet", age: "51"},
+      :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
